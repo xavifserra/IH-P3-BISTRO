f923cc3f685cc7c38a7a4a4684d520c24996bee9
Xavi <xavifserra@gmail.com>
2018-10-21 18:54:12 +0200

    reset all

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
+
 import {Switch} from 'react-router-dom'
 import PrivateRoute from './components/PrivateRoute';
 import AnonRoute from './components/AnonRoute';
 import Navbar from './components/Navbar';
 import Private from './pages/Private';
-import Signup from './pages/Auth/Signup';
-import Login from './pages/Auth/Login';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
 import AuthProvider from './components/AuthProvider';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
-//import 'spectre.css/dist/spectre.css'
+
 class App extends Component {
   render() {
     return (
       <AuthProvider>
+        <h1>Basic React Authentication</h1>
         <Navbar />
         <Switch>
           <AnonRoute path="/signup" component={Signup} />
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import auth from '../../lib/auth-service';
-import "./auth.css";
+import auth from '../lib/auth-service';
 
 class Signup extends Component {
 
@@ -37,26 +36,22 @@ class Signup extends Component {
   render() {
     const { username, password, email } = this.state;
     return (
-      <Component className='Signup'>
+      <div>
         <form onSubmit={this.handleFormSubmit}>
-
-          <Label>Username:</Label>
-          <Input type="text" name="username" value={username} onChange={this.handleChange}/>
-
-          <Label>Password:</Label>
-          <Input type="password" name="password" value={password} onChange={this.handleChange} />
-
-          <Label>email:</Label>
-          <Input type="email" name="email" value={email} onChange={this.handleChange} />
-
-          <Input type="submit" value="Signup" />
+          <label>Username:</label>
+          <input type="text" name="username" value={username} onChange={this.handleChange}/>
+          <label>Password:</label>
+          <input type="password" name="password" value={password} onChange={this.handleChange} />
+          <label>email:</label>
+          <input type="email" name="email" value={email} onChange={this.handleChange} />
+          <input type="submit" value="Signup" />
         </form>
 
         <p>Already have account?
           <Link to={"/login"}> Login</Link>
         </p>
 
-      </Component>
+      </div>
     )
   }
 }
