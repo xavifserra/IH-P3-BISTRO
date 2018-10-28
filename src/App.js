1c88ad28e192f58088ef7e76d5d208b233db65c0
Xavi <xavifserra@gmail.com>
2018-10-28 22:50:57 +0100

    firebase

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-
 import {Switch} from 'react-router-dom'
 import PrivateRoute from './components/PrivateRoute'
 import AnonRoute from './components/AnonRoute'
@@ -9,8 +8,19 @@ import Signup from './pages/Signup'
 import Login from './pages/Login'
 import AuthProvider from './components/AuthProvider'
 import PlacesProvider from './components/PlacesProvider'
+import firebase from "firebase";
 
 import './App.css'
+
+var config = {
+  apiKey: "AIzaSyDPTWpsZLlLFJ7fkt8EK0kJ8cFYR37MU9Q",
+  authDomain: "bistro-219204.firebaseapp.com",
+  databaseURL: "https://bistro-219204.firebaseio.com",
+  projectId: "bistro-219204",
+  storageBucket: "bistro-219204.appspot.com",
+  messagingSenderId: "5126862944"
+};
+firebase.initializeApp(config);
 class App extends Component {
   render() {
     return (
