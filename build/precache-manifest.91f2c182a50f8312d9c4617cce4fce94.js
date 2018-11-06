97b7b4f568a970abe4977c0514aa0f7503e12924
Xavi <xavifserra@gmail.com>
2018-11-06 19:26:22 +0100

    fixed server api

---
+++ build/precache-manifest.91f2c182a50f8312d9c4617cce4fce94.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c7df78b84de842be9b8fd20555d0d7b",
+    "url": "/static/media/gor-davtyan-725321-unsplash.6c7df78b.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9f4c38dc1a22dc6aa93c",
+    "url": "/static/js/main.9f4c38dc.chunk.js"
+  },
+  {
+    "revision": "8292c6630f11670cfe8b",
+    "url": "/static/js/1.8292c663.chunk.js"
+  },
+  {
+    "revision": "9f4c38dc1a22dc6aa93c",
+    "url": "/static/css/main.83a0b1e5.chunk.css"
+  },
+  {
+    "revision": "8292c6630f11670cfe8b",
+    "url": "/static/css/1.37f9039e.chunk.css"
+  },
+  {
+    "revision": "ce8bc7f27c9074cdd949d39000bd6499",
+    "url": "/index.html"
+  }
+];
